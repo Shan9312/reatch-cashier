cc1967856fe1041b8130620e541616ee7b5827af
qinming1217 <15377476138@163.com>
2019-03-28 09:49:18 +0800

    百度统计、native通讯、全局属性

---
+++ src/common/global.js
@@ -0,0 +1,16 @@
+/**
+ * 浏览器相关方法属性
+ */
+const Browser = {
+    name: '',
+    setName: function (name) {
+        this.name = name
+    },
+    getName: function () {
+        return this.name
+    }
+}
+
+export {
+    Browser
+}
