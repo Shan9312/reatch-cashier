2396bfaf330a2e1bcf4c489215e14ed00baa49c4
qinming1217 <15377476138@163.com>
2019-05-27 15:23:30 +0800

    babel修改

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   presets: [
     ['@vue/app', {
-      useBuiltIns: 'entry',
-      polyfills: [
-        'es6.promise',
-        'es6.symbol'
-      ]
+      useBuiltIns: 'entry'
     }]
   ]
 }
