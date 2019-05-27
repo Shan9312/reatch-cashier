d61ff5c945080291db8032e5c40738b28fc9da6c
qinming1217 <15377476138@163.com>
2019-05-27 15:54:03 +0800

    替换babel-polufill为@babel/polyfill

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import Cookies from 'js-cookie'
 import '@/common/mobileAdapt' // 移动适配
 import 'mint-ui/lib/style.css' // mint 样式
-import 'babel-polyfill' // es6 语法编译
+import '@babel/polyfill' // es6 语法编译
 import './error-log'
 
 Vue.config.productionTip = false;
