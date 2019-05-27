06f33e672b8e9cd5e873b3a16c2a107cbd23d278
haoshan <1044917411@qq.com>
2019-05-27 11:07:33 +0800

    修改babel配置 解决遍布安卓低版本显示问题

--- vue.config.js
+++ vue.config.js
@@ -14,6 +14,12 @@ module.exports = {
                 args[0].commonResourcePath = process.env.COMMON_RESOURCE_PATH // set common resource path
                 return args
             })
+        config.module
+            .rule('iview')
+            .test(/iview.src.*?js$/)
+            .use('babel')
+            .loader('babel-loader')
+            .end()
 
     },
     devServer: {
