061eda5de8b5c82e12e4921f861d16e5e600edf1
qinming1217 <15377476138@163.com>
2019-06-19 11:17:10 +0800

    银联支付

--- src/views/payment/pay/index.vue
+++ src/views/payment/pay/index.vue
@@ -1233,7 +1233,7 @@ export default {
           this.apliyPayOrder(res.data);
         } else if (this.payType === 14) {
           // 云闪付支付
-          this.applePayOrder(res.data);
+          this.unionPay(res.data);
         }
       } else {
         // 订单 无效 则返回数据 做弹窗 提示 信息
@@ -1340,12 +1340,8 @@ export default {
     },
 
     // 云闪付支付跳转接口
-    applePayOrder(data) {
-      let form = data.unionPayUrl;
-      let div = document.createElement("div");
-      div.innerHTML = form;
-      document.body.appendChild(div);
-      document.all.pay_form.submit();
+    unionPay(data) {
+      dooolyAPP.appPay(data, "pay_callBack", "union");
     },
 
     // 若是 微信环境 则微信接口跳转支付接口
